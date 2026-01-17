4ddf38f26a8b7d851c3fb2479e5f2b6a29482b58
Jimmeh <tanjimahmedz2f5@gmail.com>
2026-01-17 19:49:51 +0600

    Did Practice Problem One: Square Area Determination, No help required.

--- practiceProblems.c
+++ practiceProblems.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+
+//Practice One: Square Area Determination, No help required.
+    float sideLenght;
+    printf("Enter The lenght of one side of the square: ");
+    scanf("%f", &sideLenght);
+    printf("The area of the square is %f square units\n", sideLenght * sideLenght); 
+}
