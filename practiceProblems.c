81469258f0477ad894de9bf43efaa0ea58c51a38
Jimmeh <tanjimahmedz2f5@gmail.com>
2026-01-17 19:59:58 +0600

    Done Practice Problem two, Circle Area Calculation, No Help Required.

--- practiceProblems.c
+++ practiceProblems.c
@@ -2,9 +2,23 @@
 
 int main() {
 
-//Practice One: Square Area Determination, No help required.
+/*
+    //Practice One: Square Area Determination, No help required.
+ 
     float sideLenght;
     printf("Enter The lenght of one side of the square: ");
     scanf("%f", &sideLenght);
     printf("The area of the square is %f square units\n", sideLenght * sideLenght); 
+*/
+
+/*
+    //Pratice Two: Circle Area Determination, No help required.
+    const float pi = 3.14159;
+    float radius;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+    printf("the Area of the circle is %f square units\n", pi * radius * radius);
+*/
+
+    return 0; 
 }
